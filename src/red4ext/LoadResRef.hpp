db42f903a4e7604696dc9e7f0a3237842bc94ee5
Jack Humbert <jack.humb@gmail.com>
2023-02-02 16:44:21 -0500

    update for 1.61hf1

--- src/red4ext/LoadResRef.hpp
+++ src/red4ext/LoadResRef.hpp
@@ -2,7 +2,9 @@
 #include <RED4ext/NativeTypes.hpp>
 
 // 48 89 5C 24 08 48 89 74 24 10 57 48 83 EC 60 41 0F B6 D8 48 8B FA 48 8B F1 48 C7 44 24 20 00 00
-constexpr uintptr_t LoadResRefAddr = 0x204390;
+// 1.61 RVA: 0x204390
+// 1.62 RVA: 0x204A90
+constexpr uintptr_t LoadResRefAddr = 0x204A90;
 
 template <typename T>
 RED4ext::RelocFunc<RED4ext::ResourceToken<T> *(*)(RED4ext::ResourcePath *,
--- src/red4ext/Main.cpp
+++ src/red4ext/Main.cpp
@@ -73,7 +73,8 @@ void GetInstanceScripts(RED4ext::IScriptable *aContext, RED4ext::CStackFrame *aF
 // 48 8B C4 48 89 48 08 55 41 55 48 8D 68 A8 48 81 EC 48 01 00 00 48 89 58 10 0F 57 C0 48 89 70 E8
 // 1.6  RVA: 0x259E440
 // 1.61 RVA: 0x259F3C0
-constexpr uintptr_t UpdateNavPathAddr = 0x259F3C0;
+// 1.62 RVA: 0x259FAF0
+constexpr uintptr_t UpdateNavPathAddr = 0x259FAF0;
 
 void UpdateNavPath(RED4ext::game::ui::MinimapContainerController *, __int64, unsigned __int8,
                    RED4ext::ink::WidgetReference *);
@@ -167,7 +168,7 @@ RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::
     // is not initalized yet.
 
     Utils::CreateLogger();
-    spdlog::info("[RED4ext] Starting up In-World Navigation v0.0.5");
+    spdlog::info("[RED4ext] Starting up In-World Navigation v0.0.6");
 
     RED4ext::RTTIRegistrator::Add(RegisterTypes, PostRegisterTypes);
 
@@ -193,7 +194,7 @@ RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle, RED4ext::
 RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo *aInfo) {
   aInfo->name = L"In-World Navigation";
   aInfo->author = L"Jack Humbert";
-  aInfo->version = RED4EXT_SEMVER(0, 0, 5);
+  aInfo->version = RED4EXT_SEMVER(0, 0, 6);
   aInfo->runtime = RED4EXT_RUNTIME_LATEST;
   aInfo->sdk = RED4EXT_SDK_LATEST;
 }
