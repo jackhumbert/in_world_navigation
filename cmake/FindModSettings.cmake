7ea057a2ebb92caf49276b02bbbf9a7851f39cce
Jack Humbert <jack.humb@gmail.com>
2023-06-21 23:27:30 -0400

    move finds to lib

--- cmake/FindModSettings.cmake
+++
@@ -1,5 +0,0 @@
-message(STATUS "Configuring Mod Settings")
-list(APPEND MOD_REQUIREMENTS "Mod Settings")
-list(APPEND CMAKE_MESSAGE_INDENT "  ")
-add_subdirectory(${PROJECT_SOURCE_DIR}/deps/mod_settings)
-list(POP_BACK CMAKE_MESSAGE_INDENT)
